2503a5223efc0221be23515796269f7cdc68510c
SOLZY <65271008+so1gging@users.noreply.github.com>
2023-10-04 19:58:48 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,8 @@
+let input = require('fs').readFileSync('/dev/stdin').toString();
+const a = parseInt(input)
+
+if ((a% 4 == 0 && a%100 !== 0) || a%400 == 0) {
+  console.log(1)
+}else {
+  console.log(0)
+}
