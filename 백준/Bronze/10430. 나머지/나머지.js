dc623710be286ad9bb562f016ae3a5860fa8acb0
SOLZY <65271008+so1gging@users.noreply.github.com>
2023-10-04 20:34:49 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,8 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+const a = parseInt(input[0])
+const b = parseInt(input[1])
+const c = parseInt(input[2])
+console.log((a+b)%c)
+console.log(((a%c)+(b%c))%c)
+console.log(a*b%c)
+console.log(((a%c)*(b%c))%c)
