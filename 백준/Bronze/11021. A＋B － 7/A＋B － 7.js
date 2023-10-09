2e82ea2a3825f69ffce87a15c9d391b80f722079
SOLZY <65271008+so1gging@users.noreply.github.com>
2023-10-09 13:42:02 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 9512 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const tastcase = Number(input[0])
+let anwer = ''
+for(let i = 1 ; i <= tastcase; i++) {
+  const sum = Number(input[i].split(' ')[0]) + Number(input[i].split(' ')[1])
+  console.log('Case #'+i+': ' + sum)
+}
+
+
+
