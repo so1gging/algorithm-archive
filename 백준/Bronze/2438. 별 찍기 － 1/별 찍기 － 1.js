519ec29c0716685193c4d86b992e2d6493c9cde2
SOLZY <65271008+so1gging@users.noreply.github.com>
2023-10-09 13:45:28 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 132 ms, Memory: 9636 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const tastcase = Number(input)
+
+for(let i = 1 ; i <= tastcase; i++) {
+let star = ''
+  for (let j = 1; j<=i;j++) {
+  star+='*'
+  }
+  console.log(star)
+}
