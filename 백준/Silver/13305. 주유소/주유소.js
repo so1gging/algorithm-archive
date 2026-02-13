c73cf8e74e4c9f48d21818398115009e7922e72f
Kim solji <65271008+so1gging@users.noreply.github.com>
2026-02-13 18:06:02 +0900

    [Silver III] Title: 주유소, Time: 272 ms, Memory: 45092 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,35 +1,23 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const n = Number(input[0])
-const lengths = input[1].split(' ').map(Number)
-const prices = input[2].split(' ').map(Number)
+let cost = BigInt(0);
+const N = Number(input[0]); // N: 도시의 개수
+const distance = input[1]
+  .trim()
+  .split(" ")
+  .map((element) => BigInt(element)); // distance: 각 도시 사이의 도로 길이
 
-let totalCost = 0
-let currentFuel = 0  // 현재 남은 기름
+let city = input[2]
+  .trim()
+  .split(" ")
+  .map((element) => BigInt(element)); // city : 도시마다 주유소의 리터당 가격
 
-// 마지막 도시는 주유할 필요 없음 (도착지이므로)
-for (let i = 0; i < n - 1; i++) {
-  // 다음 도시로 가기 위해 기름 소비
-  currentFuel -= lengths[i]
-  
-  // 기름이 부족하면 주유해야 함
-  if (currentFuel < 0) {
-    // 현재 도시에서 얼마나 주유할지 계산
-    let fuelToAdd = -currentFuel  // 일단 부족한 만큼은 필수
-    
-    // "더 싼 주유소"가 나올 때까지의 거리 계산
-    for (let j = i + 1; j < n - 1; j++) {
-      // 더 싼 주유소 발견!
-      if (prices[j] < prices[i]) {
-        break
-      }
-      // 더 비싸거나 같으면, 여기까지 주유
-      fuelToAdd += lengths[j]
-    }
-    
-    totalCost += prices[i] * fuelToAdd
-    currentFuel += fuelToAdd
+let lowestPrice = city[0]; // 처음에는 맨 처음 값을 최저가를 지정
+for (let i = 0; i < city.length - 1; i++) {
+  if (lowestPrice > city[i]) {
+    //  현재 값이 최저가보다 작다면 최저가를 갱신해준다.
+    lowestPrice = city[i];
   }
+  cost += lowestPrice * distance[i];
 }
-
-console.log(totalCost)
+console.log(cost.toString());
